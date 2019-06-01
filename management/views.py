298faa23a8db1ba1874cc5235c4c944cb3dc1d1c
Dylan Martins <andrademdylan@gmail.com>
2019-06-01 18:21:51 -0300

    Add django rest framework to requirements and implement serializers

--- management/views.py
+++ management/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Employee
+from .serializers import EmployeeSerialiser
 
 # Create your views here.
+class EmployeeList(generics.ListCreateAPIView):
+
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerialiser

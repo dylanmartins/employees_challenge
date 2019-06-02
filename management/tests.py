eaaa6fe1153a54a7281e2acf8532f0a1be8951bc
Dylan Martins <andrademdylan@gmail.com>
2019-06-02 16:05:29 -0300

    Creating some view tests

--- management/tests.py
+++ management/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from model_mommy import mommy
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+from management.models import Employee
 
-# Create your tests here.
+class EmployeeIndexView(TestCase):
+    
+    def setUp(self):
+        self.url = reverse('home')
+        self.client = Client()
+        mommy.make('management.Employee', _quantity=10)
+
+    def tearDown(self):
+        Employee.objects.all().delete()
+
+    def test_view_ok(self):
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+    
+    def test_context(self):
+        response = self.client.get(self.url)
+        self.assertTrue('employees' in response.context)
+        employees = response.context.get('employees')
+        self.assertEquals(employees.count(), 10)
